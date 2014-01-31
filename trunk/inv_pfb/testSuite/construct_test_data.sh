08675cee273a1f1f4d8808de27c6e4b44c76abec
bennahugo@gmail.com <bennahugo@gmail.com@293c0209-1968-f84b-2ffd-df914e1ce66d>
2014-01-31 08:48:30 +0000

    updated the test suite (and correlated ipfb output) to store only N/2+1 non-redundant samples.

--- trunk/inv_pfb/testSuite/construct_test_data.sh
+++ trunk/inv_pfb/testSuite/construct_test_data.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+#tweak these variables if you must:
 N=512
 P=8
-num_samples_to_use=`expr $N \* $P \* 105`
+num_samples_to_use=$((N * P * 10))
 output_directory=../data_out
 tone_type=noise
+
+#don't tweak these formulas unless essential:
+non_redundant_samples=$((N / 2 + 1)) #N/2 + 1 non-redundant samples in the fft output of the pfb, by the Hermite-symmetric property of real FFTs
+non_redundant_pfb_output=$(( (num_samples_to_use / N) * non_redundant_samples ))
 
 if [ -d "$output_directory" ]; then
 		rm -r $output_directory		
 fi
 
 mkdir $output_directory
-echo -e "--------------------------------------\nDumping test data into: $output_directory\nTest data characteristics:\n--------------------------------------\nTone type: $tone_type\nN: $N\nP: $P\nNumber of samples: $num_samples_to_use"
+stats="Test data characteristics:\n--------------------------------------\nTone type: $tone_type\nN: $N\nP: $P\nNumber of samples in tone: $num_samples_to_use\nNumber of non-redundant samples in pfb output: $non_redundant_pfb_output"
+echo -e "--------------------------------------\nDumping test data into: $output_directory\n$stats"
+
 #invoke scripts
 python tone_generator.py $output_directory/noise.dat $num_samples_to_use $tone_type $N $P
 python filter_generator.py $output_directory/prototype_FIR.dat $N $P
 python pfb_generator.py $output_directory/noise.dat $output_directory/prototype_FIR.dat $output_directory/pfb.dat $output_directory/unfiltered_ffts.dat $num_samples_to_use $N $P
-python ipfb_generator.py $output_directory/pfb.dat $output_directory/prototype_FIR.dat $output_directory/py_ifftedPFB.dat $output_directory/py_ipfb.dat $num_samples_to_use $N $P
-echo -e "Test data characteristics:\n--------------------------------------\nTone type: $tone_type\nN: $N\nP: $P\nNumber of samples: $num_samples_to_use" > $output_directory/test_data_characteristics.txt
+python ipfb_generator.py $output_directory/pfb.dat $output_directory/prototype_FIR.dat $output_directory/py_ifftedPFB.dat $output_directory/py_ipfb.dat $non_redundant_pfb_output $N $P
+echo -e "$stats" > $output_directory/test_data_characteristics.txt
+
--- trunk/inv_pfb/testSuite/pfb_generator.py
+++ trunk/inv_pfb/testSuite/pfb_generator.py
@@ -18,31 +18,41 @@ out_unfiltered_file = sys.argv[4]
 no_samples = int(sys.argv[5])
 N = int(sys.argv[6])
 P = int(sys.argv[7])
+assert(no_samples % N == 0) # make sure that we can process the data in complete blocks of size N
 
 tone = np.fromfile(in_file,dtype=np.float32)
 w = np.fromfile(filter_file,dtype=np.float32).reshape(P,N)
+
 '''      
 FORWARD PFB
 '''
 pad = N*P      
+non_redundant_count = N/2 + 1 #take only the non-redundant samples (N/2 + 1), by the Hermite-symmetric property of real FFTs, and store'em
 print ">>>Computing forward PFB"
 pfb_input = np.zeros(no_samples + pad).astype(np.float32)
     
 pfb_input[pad:pad+no_samples] = tone
 pfb_filtered_output = np.zeros(no_samples).astype(np.float32)
-pfb_output = np.zeros(no_samples).astype(np.complex64)
+pfb_output = np.zeros((no_samples/N)*(non_redundant_count)).astype(np.complex64)
 for lB in range(0,no_samples,N):
     pfb_filtered_output[lB:lB+N] = (pfb_input[lB:lB+(P*N)].reshape(P,N)*w).sum(axis=0)
-    #normalize the FFT output according to Parseval's Theorem, otherwise we'll be missing a scaling factor in other IFFT implementations than that provided in numpy
-    pfb_output[lB:lB+N] = np.fft.fft(pfb_filtered_output[lB:lB+N]) / N 
+    #we're only storing N/2 + 1 samples to the final output so we have to increment the LHS with N/2 + 1 and the RHS by N
+    output_array_lB = (lB/N)*(non_redundant_count)
+    output_array_uB = output_array_lB + non_redundant_count
+    #normalize the FFT output according to Parseval's Theorem, otherwise we'll be missing a scaling factor in IFFT implementations other than that provided in numpy
+    pfb_output[output_array_lB:output_array_uB] = np.fft.fft(pfb_filtered_output[lB:lB+N])[:non_redundant_count] / N 
        
 print ">>>Computing the comparison unfiltered SFFT"
 '''
 take Short Time FFTs without filtering for comparison
 '''
-unfiltered_output = np.zeros(no_samples).astype(np.complex64)
+unfiltered_output = np.zeros((no_samples/N)*(non_redundant_count)).astype(np.complex64)
 for lB in range(0,no_samples,N):
-    unfiltered_output[lB:lB+N] = np.fft.fft(pfb_input[lB:lB+N])
+    #same as before this time we dont filter, but also take only the non-redundant samples
+    output_array_lB = (lB/N)*(non_redundant_count)
+    output_array_uB = output_array_lB + non_redundant_count	
+    unfiltered_output[output_array_lB:output_array_uB] = np.fft.fft(pfb_input[lB:lB+N])[:non_redundant_count] / N
+
 '''
 dump the output
 '''
