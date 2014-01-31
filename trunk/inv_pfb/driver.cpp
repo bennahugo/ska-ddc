66c89e48bb5f6385eef50b6071c75a720e424b16
bennahugo@gmail.com <bennahugo@gmail.com@293c0209-1968-f84b-2ffd-df914e1ce66d>
2014-01-31 09:55:58 +0000

    updated cuda version to read only non-redundant samples as output by new python pfb..

--- trunk/inv_pfb/driver.cpp
+++ trunk/inv_pfb/driver.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <sys/stat.h>
 #include "inv_pfb.h"
 
 uint32_t readDataFromDisk(const char * filename, uint32_t element_size, uint32_t length, void * buffer, uint32_t seek = 0);
@@ -46,32 +47,26 @@ uint32_t writeDataToDisk(const char * filename, uint32_t element_size, uint32_t 
 	return elemsWrote;
 }
 
-/**
-TODO: THIS IS FOR DEBUGGING PURPOSES ONLY... the real input data should not have redundant samples!
-*/
-
-void cutDuplicatesFromData(const complex_float * in, complex_float * out, uint32_t orig_count){
-        assert(orig_count % N == 0); //only whole blocks should be processed
-        for (uint32_t i = 0; i < orig_count; i += N){
-                memcpy(out + (i/N)*(N/2 + 1), in + i, (N/2 + 1) * sizeof(complex_float));
-        }
-}
-
 int main ( int argc, char ** argv ){
         char * taps_filename;
         char * pfb_output_filename;
         char * output_filename;
         uint32_t num_samples;
-        if (argc != 5){
-                fprintf(stderr, "expected arguements: 'prototype_filter_file' 'no_complex_to_read' 'pfb_output_file' 'output_file'\n");
+        if (argc != 4){
+                fprintf(stderr, "expected arguements: 'prototype_filter_file' 'pfb_output_file' 'output_file'\n");
                 return 1;
         }
         taps_filename = argv[1];
-        num_samples = atoi(argv[2]);
-	assert(num_samples % N == 0); //TODO: this is for debugging only, it should become N/2 + 1 when dealing with actual pfb data
-        pfb_output_filename = argv[3];
-        output_filename = argv[4];
-        printf("Performing operation on %d samples from '%s'\n",num_samples,pfb_output_filename);
+	pfb_output_filename = argv[2];
+        output_filename = argv[3];
+	
+	struct stat st;
+	stat(pfb_output_filename, &st);
+	uint32_t input_file_size = st.st_size; //in bytes
+	assert(input_file_size % sizeof(complex_float) == 0); //ensure we're at least dealing with a file that can be interpreted as a file of complex 32-bit floating point numbers
+        num_samples = input_file_size / sizeof(complex_float);
+	assert(num_samples % FFT_SIZE == 0); //ensure we're only processing an integral number of non-redundant real FFT samples
+        printf("Performing operation on %d blocks of complex non-redundant FFT samples (total of %d complex elements) from '%s'\n",num_samples/FFT_SIZE,num_samples,pfb_output_filename);
 
         //Read in taps file (prototype filter)  
         float * taps = (float*) malloc(sizeof(float)*WINDOW_LENGTH);
@@ -81,28 +76,26 @@ int main ( int argc, char ** argv ){
         }
 
         //Read in input file (output of earlier pfb process, so these will be complex numbers
-        complex_float * pfb_data = (complex_float*) malloc(sizeof(complex_float)*(num_samples)); //pad the array with N*P positions
+        complex_float * pfb_data = (complex_float*) malloc(sizeof(complex_float)*(num_samples));
         if (readDataFromDisk(pfb_output_filename,sizeof(complex_float),num_samples,pfb_data) != num_samples){
-                fprintf(stderr, "input pfb data does not contain %d samples\n", num_samples);
+                fprintf(stderr, "input pfb data does not contain %d non-redundant samples\n", num_samples);
                 return 1;
 	}
-        uint32_t trimmed_input_length = (N/2+1)*(num_samples / N);
-        complex_float * trimmed_pfb_data = (complex_float*) malloc(sizeof(complex_float)*trimmed_input_length);
-        cutDuplicatesFromData(pfb_data,trimmed_pfb_data,num_samples);
 
         //Setup the device and copy the taps
         initDevice(taps);
-        float * output = (float*) malloc(sizeof(float)*num_samples);
+	uint32_t ipfb_output_size = (num_samples / FFT_SIZE) * N; //The iPFB process produces blocks of size N
+        float * output = (float*) malloc(sizeof(float)*ipfb_output_size);
 
         //do some processing
-	printf("\033[0;31mProcessing starting\033[0m\n---------------------------------------------------------\n%d non-redundant samples in the input data\n",trimmed_input_length);
-	uint32_t num_loops = (uint32_t)ceil(trimmed_input_length / float(LOOP_LENGTH));
+	printf("\033[0;31mProcessing starting\033[0m\n---------------------------------------------------------\n%d non-redundant samples in the input data\n",num_samples);
+	uint32_t num_loops = (uint32_t)ceil(num_samples / float(LOOP_LENGTH));
 	printf("Only %d samples can be processed by the GPU at a time. %d iterations needed to complete the inverse pfb\n",LOOP_LENGTH,num_loops);
 	printf("---------------------------------------------------------\n");
 	for (uint32_t i = 0; i < num_loops; ++i){
-		uint32_t num_blocks_to_process = ((uint32_t)fmin(trimmed_input_length - i * LOOP_LENGTH,LOOP_LENGTH)) / FFT_SIZE; 
+		uint32_t num_blocks_to_process = ((uint32_t)fmin(num_samples - i * LOOP_LENGTH,LOOP_LENGTH)) / FFT_SIZE; 
 		printf("\033[0;32mExecuting loop %d/%d. Processing %d blocks of size %d complex numbers\033[0m\n\n",i+1,num_loops,num_blocks_to_process,FFT_SIZE);
-	        processNextStride(trimmed_pfb_data + (i * LOOP_LENGTH), output, num_blocks_to_process);
+	        processNextStride(pfb_data + (i * LOOP_LENGTH), output, num_blocks_to_process);
         	writeDataToDisk(output_filename,sizeof(float),num_blocks_to_process*N,output,i == 0);
 	}
 	printf("---------------------------------------------------------\n");
@@ -113,6 +106,5 @@ int main ( int argc, char ** argv ){
         free(output);
         free(pfb_data);
         free(taps);
-        free(trimmed_pfb_data);
         printf("\033[1;33mApplication Terminated Normally\033[0m\n---------------------------------------------------------\n");
 }
