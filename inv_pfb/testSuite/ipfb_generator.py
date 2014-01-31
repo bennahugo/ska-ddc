e7a674adfb228bea0ead85b860fce41310b1f0a6
bennahugo@gmail.com <bennahugo@gmail.com@293c0209-1968-f84b-2ffd-df914e1ce66d>
2014-01-31 08:48:30 +0000

    updated the test suite (and correlated ipfb output) to store only N/2+1 non-redundant samples.

--- inv_pfb/testSuite/ipfb_generator.py
+++ inv_pfb/testSuite/ipfb_generator.py
@@ -18,6 +18,9 @@ out_file = sys.argv[4]
 no_samples = int(sys.argv[5])
 N = int(sys.argv[6])
 P = int(sys.argv[7])
+fft_non_redundant = N / 2 + 1 #N/2+1 of non-redundant samples in a real fft by the Hermite-symmetric property
+assert(no_samples % fft_non_redundant == 0) #ensure we're only inverting an integral number of FFTs (with only non-redundant samples)
+ipfb_output_size = (no_samples/fft_non_redundant)*N
 
 pfb_output = np.fromfile(in_file,dtype=np.complex64)
 w = np.fromfile(filter_file,dtype=np.float32).reshape(P,N)
@@ -33,22 +36,25 @@ according to Daniel Zhou, A REVIEW OF POLYPHASE FILTER BANKS AND THEIR APPLICATI
 '''
 w_i = np.fliplr(w)
 
-pfb_inverse_ifft_output = np.zeros(no_samples+pad).astype(np.float32)
-pfb_inverse_output = np.zeros(no_samples).astype(np.float32)
+pfb_inverse_ifft_output = np.zeros(ipfb_output_size+pad).astype(np.float32)
+pfb_inverse_output = np.zeros(ipfb_output_size).astype(np.float32)
 
 '''
 for computational efficiency invert every FFT from the forward process only once... for xx large data
-we'll need a ring buffer to store the previous IFFTs
+we'll need a persistant buffer / ring buffer to store the previous IFFTs -- the buffering approach is explained and implemented in the CUDA version
 '''
-for lB in range(0,no_samples,N):
+for lB in range(0,no_samples,fft_non_redundant):
+    #reverse to what we've done in the forward pfb: we jump in steps of N on the LHS and steps of N/2 + 1 on the RHS
+    output_lB = (lB/fft_non_redundant)*N + pad
+    output_uB = output_lB + N
     #reverse the scaling factor (Parseval's Theorem) that we had to add in the forward python PFB to make it compatible with the CUDA real IFFT implementation
-    pfb_inverse_ifft_output[lB+pad:lB+pad+N] = np.real(np.fft.ifft(pfb_output[lB:lB+N] * N))
+    pfb_inverse_ifft_output[output_lB:output_uB] = np.real(np.fft.irfftn(pfb_output[lB:lB+fft_non_redundant] * N))
 
 '''
 Inverse filterbank
 See discussion in ipfb GPU code
 '''
-for lB in range(0,no_samples,N): 
+for lB in range(0,ipfb_output_size,N): 
     pfb_inverse_output[lB:lB+N] = np.flipud(pfb_inverse_ifft_output[lB:lB+(P*N)].reshape(P,N)*w_i).sum(axis=0)
 
 pfb_inverse_output.astype(np.float32).tofile(out_file)
