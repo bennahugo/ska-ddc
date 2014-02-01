b0d2917d0d7541f1d0ba4451d3e2fd3fed2a7db4
bennahugo@gmail.com <bennahugo@gmail.com@293c0209-1968-f84b-2ffd-df914e1ce66d>
2014-02-01 13:00:27 +0000

    added 2-D grid to casting kernel

--- inv_pfb/inv_pfb.h
+++ inv_pfb/inv_pfb.h
@@ -12,22 +12,25 @@
 /****************************************************************************************************************
  constants
 *****************************************************************************************************************/
-const uint16_t N = 512; //Number of FFT samples
-const uint16_t P = 8; //Number of Filterbanks
+const uint16_t N = 512; //Number of FFT samples (safe to tweak)
+const uint16_t P = 8; //Number of Filterbanks (safe to tweak)
 const uint32_t FFT_SIZE = N/2 + 1; //size of each input FFT (non-redundant samples)
 const uint32_t WINDOW_LENGTH = N*P;
 const uint32_t PAD = N*P;
-const uint32_t LOOP_LENGTH = 101 * FFT_SIZE; //Size of chunk to send off to the GPU. This number must be divisable by FFT_SIZE (we should send an integral number of FFTs to the GPU).
+/*Size of chunk to send off to the GPU. Safe to tweak, **BUT**: this number must be divisable by FFT_SIZE (we should 
+send an integral number of FFTs to the GPU):
+*/
+const uint32_t LOOP_LENGTH = 101 * FFT_SIZE;
 const uint32_t BUFFER_LENGTH = LOOP_LENGTH / FFT_SIZE * N + PAD; //Number of elements in the persistant ifft output buffer
+const uint32_t MAX_NO_BLOCKS = LOOP_LENGTH / FFT_SIZE;
 
+/*Block size of the int8 to cufftReal casting kernel. 256 threads per block seem to be a magic number in CUDA that works 
+well accross different generations of cards:
+*/
+const uint16_t CASTING_THREADS_PER_BLOCK = 256;
 
 /****************************************************************************************************************
- debugging flags
-*****************************************************************************************************************/
-
-
-/****************************************************************************************************************
- necessary typedefs
+ necessary Abstract Data Types
 *****************************************************************************************************************/
 typedef struct {
         int8_t r;
